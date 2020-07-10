974b78acea17d04b8829709057bbb5113c38fbf7
darlson <davertiser@gmail.com>
2020-07-10 16:47:53 -0600

    add Login info to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import About from './About'
-import {Link} from 'react-router-dom'
-import templogo from '../images/templogo.png'
+import React, { useState } from "react";
+// import About from "./About";
+import { Link } from "react-router-dom";
+import templogo from "../images/templogo.png";
+import axios from "axios";
 
-const Nav = () => {
+const Nav = (props) => {
+  const [value, setValue] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    e.preventDefault();
+    axios
+      .post("/auth/login", { value, password })
+      .then((res) => {
+        props.loginUser(res.data);
+        props.history.push("/dashboard");
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
+  };
+
   return (
     <div>
-      <Link to='/'><img className='app-logo' src={templogo} alt='WordHunch logo' style={{width: "120px"}} /></Link>
-      <Link to='/auth'>Register</Link>
-      <Link to='/profile'>Profile</Link>
-      <About/>
+      <Link to="/">
+        <img
+          className="app-logo"
+          src={templogo}
+          alt="WordHunch logo"
+          style={{ width: "120px" }}
+        />
+      </Link>
+      <div className="not-loggedin">
+        <form onSubmit={(e) => login(e)}>
+          <input
+            className="login-input"
+            type="text"
+            placeholder="username or email"
+            value={value}
+            onChange={(e, v) => setValue(e.target.value)}
+          />
+          <input
+            className="login-input"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e, v) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+        <Link to="/auth">Register</Link>
+        
+      </div>
+      <div className="logged-in">
+        <Link to="/profile">Profile</Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Nav;
