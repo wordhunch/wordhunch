ce893ec476ed4e5d53f7c20ae678a25f6ebbbd3b
Benjamin Thomsen <benjaminthomsen@Benjamins-MacBook-Air.local>
2020-07-10 13:00:04 -0700

    Finished Routes

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,11 @@
 import React from "react"
+import {Switch, Route} from "react-router-dom"
+import Landing from "./components/Landing/Landing"
+import Profile from "./components/Profile/Profile"
+
+export default (
+    <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/profile" component={Profile} />
+    </Switch>
+)
