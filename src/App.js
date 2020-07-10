e117bcab98ae4d7bc25c75ccbab747fd712d3ce5
sammyt090 <64330760+sammyt090@users.noreply.github.com>
2020-07-10 15:15:33 -0700

    auth front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Nav from './components/Nav'
-import Landing from './components/Landing'
+import routes from './routes'
 
 function App() {
   return (
     <div className="App">
       {Nav}
-      {Landing}
+      {routes}
       App.js
     </div>
   );
