16333e8b25c4e4e21614d36b8feacbb9c5dc5ac3
Benjamin Thomsen <benjaminthomsen@Benjamins-MacBook-Air.local>
2020-07-10 10:48:46 -0700

    Initialized server/index.js

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const massive = rquire("massive");
+const express = require("express");
+const session = require("express-session");
+const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+})
+  .then((db) => {
+    app.set("db", db);
+    console.log("db connected");
+    app.listen(SERVER_PORT, () =>
+      console.log(`Listening on port ${SERVER_PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
+
+  app.use(express.static(`${__dirname}/../build`))
+  app.use(express.json())
+
+  app.use(
+      session({
+          resave: false,
+          saveUninitialized: false,
+          secret: SESSION_SECRET,
+          cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+      })
+  )
+
+  //endpoints
