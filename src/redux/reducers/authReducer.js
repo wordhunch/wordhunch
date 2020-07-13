0324bc62b1f07eca74e881a1d56fb1fca69fdc41
darlson <davertiser@gmail.com>
2020-07-13 15:18:47 -0600

    tinkering with Nav bar

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -4,6 +4,7 @@ const initialState = {
 }
 
 const SET_USER = 'SET_USER'
+const LOGOUT_USER = 'LOGOUT_USER'
 
 export const setUser = (username, userId) => {
     return {
@@ -12,12 +13,21 @@ export const setUser = (username, userId) => {
     }
 }
 
-export default (state=initialState, action) {
+export const logoutUser = (user) => {
+    return {
+        type: LOGOUT_USER,
+        payload: initialState
+    }
+}
+
+export default function (state = initialState, action) {
     switch (action.type) {
         case SET_USER:
             const {username, userId} = action.payload
             return {...state, username, userId}
+        case LOGOUT_USER:
+            return {...state, ...action.payload}
         default:
-            return state
+            return initialState
     }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,8 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import promiseMiddleware from 'redux-promise-middleware'
 import authReducer from './reducers/authReducer'
 
-const rootReducer = combineReducers({
-    auth: authReducer
-})
+// const rootReducer = combineReducers({
+//     auth: authReducer
+// })
 
-export default createStore(rootReducer, composeWithDevTools(applyMiddleware(promiseMiddleware)))
+export default createStore(authReducer, composeWithDevTools(applyMiddleware(promiseMiddleware)))
